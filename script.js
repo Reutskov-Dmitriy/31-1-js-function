a5ddf95b46d99125ef6f5bb48acb2fe0f7d35674
Reutskov-Dmitriy <infowelc@gmail.com>
2022-08-08 19:13:54 +0300

    next commit

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function checkPerfectNum(num) {
 		document.querySelector('.output').innerHTML = "It's not a Perfect Number";
 	}
 
-	for (let i = 1; i < num; i++) {
+	for (let i = 1; i <= num / 2; i++) {
 		if (num % i == 0) {
 			sum += i;
 			if (num === sum && sum !== 0) {
