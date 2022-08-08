ad6ac42a011baad921fb8dd8b8fe92e1181fa214
Reutskov-Dmitriy <infowelc@gmail.com>
2022-08-08 18:21:03 +0300

    add function innerPerfectNum

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
+
+document.querySelector('.btn_inputNum').addEventListener('click', () => {
+	let data = document.querySelector('#inputNum').value;
+	innerPerfectNum(+data)
+});
+let perfectNumber
+
+function innerPerfectNum(num) {
+	checkPerfectNum(num)
+	if (num === perfectNumber && perfectNumber !== 0) {
+		document.querySelector('.output').innerHTML = "Perfect Number";
+	} else {
+		document.querySelector('.output').innerHTML = "It's not a Perfect Number";
+	}
+}
+
+function checkPerfectNum(num) {
+	let sum = 0;
+	if (num <= 0) {
+		document.querySelector('.output').innerHTML = "It's not a Perfect Number";
+	}
+
+	for (let i = 1; i < num; i++) {
+		if (num % i == 0) {
+			sum += i;
+			if (num === sum && sum !== 0) {
+				perfectNumber = sum;
+			}
+		}
+	}
+}
+
+
 const startNumInput = document.querySelector('#inputStartNum');
 const endNumInput = document.querySelector('#inputEndNum');
 let perfectNumBtn = document.querySelector('.btn_perfectNum');
 
 perfectNumBtn.onclick = function () {
-	const numPerfect = checkPerfectNum(startNumInput.value, endNumInput.value);
+	checkPerfectNums(+startNumInput.value, +endNumInput.value);
 }
 
-function checkPerfectNum(num1, num2) {
-	console.log(num1)
-	console.log(num2)
-	let perfectNumbers = 0
+function checkPerfectNums(num1, num2) {
+	let perfectNumbers = [];
 	for (let j = num1; j < num2; j++) {
-
-		let sum = 0;
-		for (let i = 1; i < j; i++) {
-
-			if (j % i == 0) {
-				sum += i;
-				if (+j === sum) {
-					perfectNumbers = `${sum}`;
-				}
-			}
+		checkPerfectNum(j);
+		if (j === perfectNumber && perfectNumber !== 0) {
+			perfectNumbers.push(perfectNumber)
 		}
 	}
-	console.log(perfectNumbers);
 
-	if (perfectNumbers !== 0) {
+	if (perfectNumbers.length > 0) {
 		document.querySelector('#answer').innerHTML = perfectNumbers;
 	} else {
-		document.querySelector('#answer').innerHTML = "It's not a Perfect Number";
+		document.querySelector('#answer').innerHTML = "It's not a Perfect Numbers";
 	}
 }
 
